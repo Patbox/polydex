49953733f8709f7d427a2c051562d99d2f5db267
Patbox <39821509+Patbox@users.noreply.github.com>
2023-08-14 22:49:00 +0200

    Hopefully fix create crash 2

--- src/main/java/eu/pb4/polydex/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/eu/pb4/polydex/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,6 +1,7 @@
 package eu.pb4.polydex.mixin;
 
 import eu.pb4.polydex.impl.PlayerInterface;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import org.spongepowered.asm.mixin.Final;
@@ -16,7 +17,9 @@ public class ServerPlayerInteractionManagerMixin {
 
     @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerInteractionManager;continueMining(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;I)F"))
     private void polydex_updateMining(CallbackInfo ci) {
-        ((PlayerInterface) this.player.networkHandler).polydex_getTarget().onBreakingChange();
-        ((PlayerInterface) this.player.networkHandler).polydex_getDisplay().onBreakingStateUpdate();
+        if (this.player.networkHandler.getClass() == ServerPlayNetworkHandler.class) {
+            ((PlayerInterface) this.player.networkHandler).polydex_getTarget().onBreakingChange();
+            ((PlayerInterface) this.player.networkHandler).polydex_getDisplay().onBreakingStateUpdate();
+        }
     }
 }
